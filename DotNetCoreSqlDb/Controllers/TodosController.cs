22411cb00d1d7247e9bf47806f97d8995994581c
Ieinadex <daniel.mullis@gmail.com>
2023-03-22 21:17:00 -0400

    Added the AssignedTo field

--- DotNetCoreSqlDb/Controllers/TodosController.cs
+++ DotNetCoreSqlDb/Controllers/TodosController.cs
@@ -98,7 +98,7 @@ namespace DotNetCoreSqlDb.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,CreatedDate")] Todo todo)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Description,CreatedDate,AssignedTo")] Todo todo)
         {
             if (id != todo.ID)
             {
